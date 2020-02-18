b1a2743a19d8409b11bd069a6123321e20b339a3
soy-1230 <61173025+soy-1230@users.noreply.github.com>
2020-02-18 12:23:33 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+ "fmt"
+
+ "github.com/k30339035/awsgolang/AWS/Utils"
+)
+
+func main() {
+ //reverse a sentense.
+ fmt.Println(Utils.Reverse("HTAPOGgnitteS/ikiw/og/gnalog/moc.buhtig//:sptth tlusnoc dluohs sresu swodniW"))
+}
